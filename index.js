fa852e966622932b6c84c8237133b592a37e1482
raju720190r <raju720190r@gmail.com>
2023-06-15 21:53:14 +0600

    Collection Make

--- index.js
+++ index.js
@@ -1,18 +1,84 @@
-const express =require('express');
+const express = require('express');
 const app = express();
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // Midddleware 
-app.use (cors());
+app.use(cors());
 app.use(express.json());
 
 
-app.get('/',(req,res)=>{
-    res.send('Server Is Running')
+// ! verify Jwt token
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ error: true, message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// Mongodb Connection 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.D_USER}:${process.env.D_PASS}@cluster0.9k9az0h.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+  
+    // MongoDB All User Collection
+    const userCollection = client.db("learningSchool").collection("users");
+    const classCollection = client.db("learningSchool").collection("classes");
+    const selectedCollection = client.db("learningSchool").collection("selected");
+    const enrolledCollection = client.db("enroll").collection("enrolled");
+
+
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      // console.log(process.env.ACCESS_TOKEN_SECRET)
+      res.send({ token })
+  });
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Server Is Running')
 })
 
-app.listen(port,()=>{
-    console.log(`Learning School is Running On port ${port}`);
+app.listen(port, () => {
+  console.log(`Learning School is Running On port ${port}`);
 })
