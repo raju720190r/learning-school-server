7e2a9098df74476e4536795912dd2b4c0d998ee6
raju720190r <raju720190r@gmail.com>
2023-06-17 10:02:34 +0600

    Some Problem fixed

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
+const stripe = require("stripe")(process.env.PAYMENT_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
